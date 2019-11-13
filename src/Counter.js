593163318862f565976a7e44a9d26d3c745cdea2
Valentine Voyen <valentine.voyen1@gmail.com>
2019-11-13 11:32:06 +0100

    added input with hooks

--- src/Counter.js
+++ src/Counter.js
@@ -5,9 +5,20 @@ function Counter () {
   // Déclare une fonction setCount qui permet de modifier la valeur de count
 
   const [count, setCount] = useState(0);
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Value {count}`)
+  }
+  
   return (
     <div>
+          <p>Type a value: </p> 
+          <input
+          type="text"
+          value={count}
+          onChange={e => setCount(e.target.value)}
+          />
+       
       <p>Le compteur est à:{count} </p>
       <button onClick={() => setCount(count +1)}>
         +1
